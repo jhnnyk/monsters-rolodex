a73dd3961a14ce38e71f1112270539358e1336bb
John Knotts <jhnnyk@gmail.com>
2020-08-24 10:12:25 -0600

    Updates

---
+++ precache-manifest.aa8f0521302913b4aec2004fa5840de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c24fee78b91f90fc4b573dddebf95bb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f41925511ce646ecbb2a",
+    "url": "/monsters-rolodex/static/css/main.8512c9d1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41925511ce646ecbb2a",
+    "url": "/monsters-rolodex/static/js/main.e7478b0e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
